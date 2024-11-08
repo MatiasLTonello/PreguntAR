dbf5ccedf6b1d5bacc379f8f79406bc70a28a287
lautarodesouches <lautarodesouches@outlook.com>
2024-11-08 15:33:25 -0300

    [Style] Todas las vistas

--- database.sql
+++ database.sql
@@ -85,6 +85,9 @@ INSERT INTO usuarios (
     esta_verificado
 ) VALUES
       ('Juan Perez', '2000-1-01', 'test', 'test@test.com', '098f6bcd4621d373cade4e832627b4f6', 'Argentina', 1, 'test.png', 'masculino', current_timestamp(), '', '0'), -- contraseña: test
+      ('Sofia Morales', '1999-6-22', 'sofia', 'sofia@test.com', '098f6bcd4621d373cade4e832627b4f6', 'Argentina', 1, 'test.png', 'masculino', current_timestamp(), '', '0'), -- contraseña: test
+      ('Diego Rios', '2001-9-05', 'diego', 'diego@test.com', '098f6bcd4621d373cade4e832627b4f6', 'Argentina', 1, 'test.png', 'masculino', current_timestamp(), '', '0'), -- contraseña: test
+      ('Lucia Fernández', '2002-4-12', 'lucia', 'lucia@test.com', '098f6bcd4621d373cade4e832627b4f6', 'Argentina', 1, 'test.png', 'masculino', current_timestamp(), '', '0'), -- contraseña: test
       ('Leo Arias', '2000-1-01', 'leo', 'correo@correo.com', '202cb962ac59075b964b07152d234b70' , 'Argentina', 1, 'test.png', 'masculino', current_timestamp(), '', '1'); -- contraseña: 123
 
 INSERT INTO categorias (nombre, color) VALUES
